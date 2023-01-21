067c5e508e1678e451186a3e192c7d8685a4e92a
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-22 02:16:23 +0800

    Create rook file

---
+++ lib/pieces/rook.rb
@@ -0,0 +1,16 @@
+class Rook
+  attr_reader :type, :position, :symbol, :moves
+
+  def initialize(type, position)
+    @type = type
+    @position = position
+    @symbol = assign_symbol
+    @moves = []
+  end
+
+  private
+
+  def assign_symbol
+    type == 'white' ? "\u2656" : "u\265C"
+  end
+end
