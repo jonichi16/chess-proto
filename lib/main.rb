6f459607317e1fc623037b5280e2b0e3c25f5de4
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-22 01:05:46 +0800

    Refactor main

--- lib/main.rb
+++ lib/main.rb
@@ -5,20 +5,21 @@ def play
   Message.display_intro
   print 'Please type the number of your choice: '
   player_choice = Message.player_input
-  case player_choice
+  Message.clear_screen
+  option(player_choice)
+end
+
+def option(choice)
+  case choice
   when 1
-    Message.clear_screen
     start_new_game
   when 2
     # method for loading game
-    Message.clear_screen
     puts 'Loading game'
   else
-    Message.clear_screen
     puts 'Thank you for playing'
   end
 end
-
 
 def start_new_game
   Chess.new
