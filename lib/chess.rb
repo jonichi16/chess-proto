f190d54704d25b551d712d11c29a5f7db62e22ac
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-22 03:39:36 +0800

    Add content to Chess class

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+require './lib/player'
+
 # * Class for the game flow
 class Chess
+  attr_reader :board, :white_player, :black_player, :current_player
+
+  def initialize(board = Board.new)
+    @board = board
+    @white_player = Player.new('White Player', 'white')
+    @black_player = Player.new('Black Player', 'black')
+    @current_player = white_player
+  end
+
+  def display_current_board
+    white_pieces = white_player.pieces
+    black_pieces = black_player.pieces
+    board.display_board(white_pieces, black_pieces)
+  end
 end
