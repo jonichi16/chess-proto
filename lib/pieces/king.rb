0aebed41c3a06427389fae581feec89be2484567
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-22 02:49:09 +0800

    Create king

---
+++ lib/pieces/king.rb
@@ -0,0 +1,16 @@
+class King
+  attr_reader :type, :position, :symbol, :moves
+
+  def initialize(type, position)
+    @type = type
+    @position = position
+    @symbol = assign_symbol
+    @moves = []
+  end
+
+  private
+
+  def assign_symbol
+    type == 'white' ? "\u2654" : "u\265A"
+  end
+end
