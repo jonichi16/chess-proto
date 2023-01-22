1a9797fc77dbcec191cb17027e2c8e066541b4fc
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-23 01:44:11 +0800

    Inherit Piece class to pieces

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,24 +1,9 @@
 # frozen_string_literal: true
 
+require './lib/pieces/pieces'
+
 # * Class for creating bishops
-class Bishop
-  attr_reader :type, :position, :symbol, :moves
-
-  def initialize(type, position)
-    @type = type
-    @position = position
-    @symbol = assign_symbol
-    @moves = []
-  end
-
-  def to_s
-    symbol
-  end
-
-  def move_position(destination)
-    @position = destination
-  end
-
+class Bishop < Piece
   private
 
   def assign_symbol
