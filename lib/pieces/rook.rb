9a1031764674a6a0a9edbc7e23ae9d81e81cbf24
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-23 14:14:29 +0800

    Add legal moves for rooks

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
+# rubocop:disable Layout/LineLength
+ROOK_MOVES = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]].freeze
+# rubocop:enable Layout/LineLength
+
 require './lib/pieces/pieces'
 
 # * Class for creating rooks
 class Rook < Piece
+  def add_moves(position)
+    valid_moves = []
+    ROOK_MOVES.each do |move|
+      x = move[0] + position[0]
+      y = move[1] + position[1]
+      next if !x.between?(0, 7) ||
+              !y.between?(0, 7)
+
+      valid_moves.push([x, y])
+    end
+    valid_moves
+  end
+
   private
 
   def assign_symbol
